866c7ec6d9f2ee9cb544e63dc0819221a6bf06e0
Matias-DR <Matias-DR@outlook.com>
2024-01-26 17:00:22 -0300

    ADD: vercel.app configs

--- todo/settings.py
+++ todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-djp+86nu-xsdemg)v&y_5^%ddrp%#gv7f07hc1k4sxu-k%w5^k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
