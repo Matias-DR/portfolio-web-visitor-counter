d4057a95a7d54129f7e0cfffdacf8c84c6d10a3a
Matias-DR <Matias-DR@outlook.com>
2024-01-26 03:55:20 -0300

    ADD: test for web_portfolio_visitor_counter and created ecommerce-purchases app

--- web_portfolio_visitor_counter/tests.py
+++ web_portfolio_visitor_counter/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CounterViewTestCase(APITestCase):
+    def setUp(self):
+        self.data = {
+            'ip': 'i.p.e.x'
+        }
+
+    def test_post(self):
+        response = self.client.post(
+            '/api/counter/',
+            self.data,
+            format='json'
+        )
+        print(response)
+        self.assertEqual(response.status_code, 200)
--- web_portfolio_visitor_counter/views.py
+++ web_portfolio_visitor_counter/views.py
@@ -13,11 +13,25 @@ class CounterView(APIView):
         """
             Gets ip from body and executes de use case of count
         """
-        ip = request.data.get('ip', None)
-        if not ip:
-            return Response({'message': 'IP address not provided'}, status=400)
+        # Si como "data" envían cualquier cosa que NO sea un objeto JSON,
+        # se rompe, por eso las sentencias try-except para "data.get(...)".
+        try:
+            ip = request.data.get('ip', None)
+            if not ip:
+                return Response(
+                    {'message': 'IP address not provided'},
+                    status=400
+                )
+        except:
+            return Response(
+                {'message': 'IP address not provided'},
+                status=400
+            )
         try:
             res = countUseCase(ip)
             return Response(res, status=200)
         except Exception as e:
-            return Response({'message': e.msg}, status=e.status_code)
+            return Response(
+                {'message': e.msg},
+                status=e.status_code
+            )
